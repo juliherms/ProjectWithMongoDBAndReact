9bf9d9d2967ef5690ba30b56fe7e051b3e75d792
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-01-16 23:37:47 -0300

    implement check user exists

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,26 +5,31 @@ module.exports = {
     async save(request, response)  {
 
     const { link,skills,latitude,longitude } = request.body;
-    const apiResponse = await axios.get(`https://api.github.com/users/${link}`);
-    const { name = login, avatar_url, bio} = apiResponse.data;
-    const skillsArray = skills.split(',').map(skill => skill.trim());
 
-    const location = {
-        type: 'Point',
-        coordinates: [longitude,latitude],
-    };
+    //check user exists
+    let user = await User.findOne({ link });
 
-    //short sintax
-    const ret = await User.create({
-       name,
-       link,
-       description : bio,
-       avatar_url,
-       skills: skillsArray,
-       location
+    if(!user){
 
-    })
+        const apiResponse = await axios.get(`https://api.github.com/users/${link}`);
+        const { name = login, avatar_url, bio} = apiResponse.data;
+        const skillsArray = skills.split(',').map(skill => skill.trim());
 
+        const location = {
+            type: 'Point',
+            coordinates: [longitude,latitude],
+        };
+
+        //short sintax
+        const ret = await User.create({
+        name,
+        link,
+        description : bio,
+        avatar_url,
+        skills: skillsArray,
+        location
+        })
+    }
     return response.json(ret);
-    }   
+  }   
 };
